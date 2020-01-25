192286cf2d0f2e658e9e9fef8491d118b7828776
omid <omid.mesgarha@gmail.com>
2020-01-25 13:37:43 +0330

    add some fields to profile

--- accounting/models.py
+++ accounting/models.py
@@ -1,11 +1,13 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Account(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, )
     portfolio_site = models.URLField(blank=True)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    location = models.TextField(max_length=60, default='iran!', )
+    bio = models.TextField(max_length=120, default='nothing until now')
 
     def __str__(self):
         return self.user.username
