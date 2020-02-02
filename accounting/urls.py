a10f948946b2fd2256451ab1de84a3d435f5d2ec
Hasan Sendani <hasansendani1@gmail.com>
2020-02-02 23:25:22 +0330

    manipulated sth

--- accounting/urls.py
+++ accounting/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('activate/<str:username>/<str:code>', views.activate, name='activate'),
     url(r'^logout/', views.logout_view, name='logout'),
     url(r'^edit/$', views.edit_view, name='edit'),
+    url(r'^forgot/$', views.forget_password_view, name='forgot'),
+    url(r'^reset/$', views.reset_password_view, name='reset'),
     url(r'^profile/$', views.profile, name='profile')
 ]
